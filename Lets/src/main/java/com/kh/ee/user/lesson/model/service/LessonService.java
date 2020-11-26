08c5fa0c906d7b5bf4798829cba3f7110f3ba2a2
soohyun yoon <59842439+soohyunyoon@users.noreply.github.com>
2020-11-26 10:49:46 +0900

    1126_윤수현_mempay수정중

--- Lets/src/main/java/com/kh/ee/user/lesson/model/service/LessonService.java
+++ Lets/src/main/java/com/kh/ee/user/lesson/model/service/LessonService.java
@@ -9,11 +9,9 @@ import com.kh.ee.user.tutor.model.vo.Tutor;
 
 public interface LessonService {
 	
-	ArrayList<Lesson> selectApproveLessonList(Lesson l);
+	ArrayList<Lesson> selectApproveLessonList();
 	
-	ArrayList<Lesson> selectApproveStatusList(Lesson l);
-	
-	ArrayList<Lesson> selectPrepareList(Lesson l);
+	ArrayList<Lesson> selectApproveStatusList();
 	
 	// 상세페이지에 뿌려줄 내용들 (학천)
 	ArrayList<Review> selectReview(int lessonNo); 
